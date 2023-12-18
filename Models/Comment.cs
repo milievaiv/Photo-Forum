f872bdf632f08f2e65eab18547af768c5e7451f1
Ivan Kourtev <135639014+IvanKourtev@users.noreply.github.com>
2023-12-18 16:35:21 +0200

    Implemented Post and Comment Class

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -4,5 +4,10 @@ namespace PhotoForum.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        //public User User { get; set; }
+        //public int PostId { get; set; }
     }
 }
