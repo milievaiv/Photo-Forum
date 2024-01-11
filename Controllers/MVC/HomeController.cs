761128f3a101135ca9ab114cb7a70064c76b7d3c
Maria <109968941+milievaiv@users.noreply.github.com>
2024-01-11 09:55:42 +0200

    Add grid gallery to home controller

---
+++ Controllers/MVC/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoForum.Controllers.MVC
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
